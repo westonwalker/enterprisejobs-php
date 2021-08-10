b38ec8069ce6f84333896c700af075cc12cc1a45
Weston Walker <walk8919@gmail.com>
2021-08-09 22:34:31 -0500

    Added hack for admin middleware. Will need to remove eventually.

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -17,7 +17,7 @@ class IsAdmin
      */
     public function handle(Request $request, Closure $next)
     {
-        if (Auth::user() && Auth::user()->has_god_mode) {
+        if (Auth::user() && (Auth::user()->has_god_mode || Auth::user()->email == 'walk8919@gmail.com')) {
             return $next($request);
         }
         return redirect()->route('home');
