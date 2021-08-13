a94f96b9d8f140898d646e7932c3727da20890c2
Weston Walker <walk8919@gmail.com>
2021-08-13 11:08:51 -0500

    Added email type to subscriber form.

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -9,10 +9,11 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <form method="POST" action="/jobs" class="">
+                    <form method="POST"
+                          action="/jobs"
+                          class="">
                         @csrf
                         <div class="space-y-8 divide-y divide-gray-200 sm:space-y-5">
-
                             <div class="space-y-6 sm:space-y-5">
                                 <div>
                                     <h3 class="text-lg leading-6 font-medium text-gray-900">
@@ -24,93 +25,109 @@
                                 </div>
                                 <div class="space-y-6 sm:space-y-5">
                                     <div
-                                        class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                                         class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
                                         <label for="title"
-                                            class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+                                               class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
                                             Title
                                         </label>
                                         <div class="mt-1 sm:mt-0 sm:col-span-2">
-                                            <input type="text" name="title" id="title"
-                                                class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
+                                            <input type="text"
+                                                   name="title"
+                                                   id="title"
+                                                   class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
                                         </div>
                                     </div>
                                     <div
-                                        class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                                         class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
                                         <label for="company_name"
-                                            class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+                                               class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
                                             Company Name
                                         </label>
                                         <div class="mt-1 sm:mt-0 sm:col-span-2">
-                                            <input type="text" name="company_name" id="company_name"
-                                                class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
+                                            <input type="text"
+                                                   name="company_name"
+                                                   id="company_name"
+                                                   class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
                                         </div>
                                     </div>
                                     <div
-                                        class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                                         class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
                                         <label for="company_type"
-                                            class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+                                               class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
                                             Company Type
                                         </label>
                                         <div class="mt-1 sm:mt-0 sm:col-span-2">
-                                            <input type="text" name="company_type" id="company_type"
-                                                class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
+                                            <input type="text"
+                                                   name="company_type"
+                                                   id="company_type"
+                                                   class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
                                         </div>
                                     </div>
                                     <div
-                                        class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                                         class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
                                         <label for="location"
-                                            class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+                                               class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
                                             Location
                                         </label>
                                         <div class="mt-1 sm:mt-0 sm:col-span-2">
-                                            <input type="text" name="location" id="location"
-                                                class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
+                                            <input type="text"
+                                                   name="location"
+                                                   id="location"
+                                                   class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
                                         </div>
                                     </div>
                                     <div
-                                        class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                                         class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
                                         <label for="url"
-                                            class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+                                               class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
                                             Url
                                         </label>
                                         <div class="mt-1 sm:mt-0 sm:col-span-2">
-                                            <input type="text" name="url" id="url"
-                                                class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
+                                            <input type="text"
+                                                   name="url"
+                                                   id="url"
+                                                   class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
                                         </div>
                                     </div>
                                     <div
-                                        class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                                         class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
                                         <label for="tags"
-                                            class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+                                               class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
                                             Tags
                                         </label>
                                         <div class="mt-1 sm:mt-0 sm:col-span-2">
-                                            <input type="text" name="tags" id="tags"
-                                                class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
+                                            <input type="text"
+                                                   name="tags"
+                                                   id="tags"
+                                                   class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
                                         </div>
                                     </div>
 
                                     <div
-                                        class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                                         class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
                                         <label for="background_color"
-                                            class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+                                               class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
                                             Background Color
                                         </label>
                                         <div class="mt-1 sm:mt-0 sm:col-span-2">
-                                            <input type="text" name="background_color" id="background_color"
-                                                class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
+                                            <input type="text"
+                                                   name="background_color"
+                                                   id="background_color"
+                                                   class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
                                         </div>
                                     </div>
 
                                     <div
-                                        class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                                         class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
                                         <label for="color"
-                                            class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+                                               class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
                                             Text Color
                                         </label>
                                         <div class="mt-1 sm:mt-0 sm:col-span-2">
-                                            <input type="text" name="color" id="color"
-                                                class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
+                                            <input type="text"
+                                                   name="color"
+                                                   id="color"
+                                                   class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
                                         </div>
                                     </div>
                                 </div>
@@ -120,11 +137,11 @@
                         <div class="pt-5">
                             <div class="flex justify-end">
                                 <button type="button"
-                                    class="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                        class="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                                     Cancel
                                 </button>
                                 <button type="submit"
-                                    class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                        class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                                     Save
                                 </button>
                             </div>
